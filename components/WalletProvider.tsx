411c616e6821c1de04f57ce62cd507ad8257e4ba
klikmarkettt-dotcom <klikmarkettt@gmail.com>
2026-05-19 08:00:55 +0200

    Create WalletProvider.tsx

---
+++ components/WalletProvider.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { useMemo } from 'react'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+
+require('@solana/wallet-adapter-react-ui/styles.css')
+
+const RPC = process.env.NEXT_PUBLIC_RPC_URL || 'https://api.mainnet-beta.solana.com'
+
+export function SolanaWalletProvider({ children }: { children: React.ReactNode }) {
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
+  return (
+    <ConnectionProvider endpoint={RPC}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
