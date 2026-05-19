376761a53023216707b29d44a0b7f01dce22c050
klikmarkettt-dotcom <klikmarkettt@gmail.com>
2026-05-19 07:59:14 +0200

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const CREATOR_WALLET = 'Ghz2RotTtZKJeUFFNVfYV8NrB6TW5ZkJKQGcVYx31PvD'
+export const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL || 'https://api.mainnet-beta.solana.com'
+export const SOLSCAN_BASE = 'https://solscan.io/tx'
