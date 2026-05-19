57c240e87f82f0058c11f223fcd7274c97189bb0
klikmarkettt-dotcom <klikmarkettt@gmail.com>
2026-05-19 08:22:16 +0200

    Import Solana wallet adapter styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import '@solana/wallet-adapter-react-ui/styles.css'
 import { SolanaWalletProvider } from '@/components/WalletProvider'
 
 export const metadata: Metadata = {
